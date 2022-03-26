827f5c7f4d78fa85c137dd03e274993fc5f141d9
Gabriel <gabriellima.glgs@gmail.com>
2022-03-25 22:38:21 -0300

    homepage and listview

---
+++ lib/models/movie.dart
@@ -0,0 +1,17 @@
+class Movie {
+  String nome;
+  String cartaz;
+  String data;
+  String trailer;
+  String genero;
+  String tempo;
+
+  Movie({
+    required this.nome,
+    required this.cartaz,
+    required this.data,
+    required this.trailer,
+    required this.genero,
+    required this.tempo
+});
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class HomePage extends StatelessWidget{
   const HomePage({Key? key}) : super(key: key) ;
@@ -9,6 +12,29 @@ class HomePage extends StatelessWidget{
       appBar: AppBar(
         title: Text('Home'),
       ),
+      body : StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection("Movies").snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return Text('Loading...');
+          return ListView.separated(
+            separatorBuilder: (context, snapshot) => Divider(),
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+
+                return ListTile(
+                  leading: Image.network(snapshot.data!.docs[index]['cartaz']),
+                  title: Text(snapshot.data!.docs[index]['nome'], style: new TextStyle(color: Colors.white),),
+                  subtitle: Text(snapshot.data!.docs[index]['genero'], style: new TextStyle(color: Colors.white)),
+                  tileColor: Colors.blueGrey,
+
+                );
+            },
+            padding: EdgeInsets.all(16),
+
+
+          );
+        },
+      )
     );
   }
 }
